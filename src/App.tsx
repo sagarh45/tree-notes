a9b834f229dd4041c42d01d5abd05bb5ed21c25e
sagarh45 <sagarhiremath12@gmail.com>
2026-09-12 09:34:10 +0530

    Route all syllabus headings through the new tabbed course page

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,37 @@
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
 import { AppShell } from './layout/AppShell'
-import { SyllabusPage } from './pages/SyllabusPage'
+import { CoursePage } from './pages/CoursePage'
 import { LabPage } from './pages/LabPage'
-import { ProgramsPage } from './pages/ProgramsPage'
 import { PracticePage } from './pages/PracticePage'
 import { RevisionPage } from './pages/RevisionPage'
+import { ProgramsPage } from './pages/ProgramsPage'
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route element={<AppShell />}>
-          <Route index element={<Navigate to="/definition" replace />} />
+          <Route index element={<CoursePage />} />
 
-          <Route path="definition" element={<SyllabusPage />} />
-          <Route path="traversal" element={<SyllabusPage />} />
-          <Route path="linked-implementation" element={<SyllabusPage />} />
-          <Route path="binary-tree-operations" element={<SyllabusPage />} />
-          <Route path="bst-operations" element={<SyllabusPage />} />
-          <Route path="multiway-trees" element={<SyllabusPage />} />
-          <Route path="b-trees" element={<SyllabusPage />} />
-          <Route path="avl-tree" element={<SyllabusPage />} />
-          <Route path="single-rotation" element={<SyllabusPage />} />
-          <Route path="double-rotation" element={<SyllabusPage />} />
-
-          <Route path="theory" element={<Navigate to="/definition" replace />} />
-          <Route path="overview" element={<Navigate to="/definition" replace />} />
+          <Route path="theory" element={<Navigate to="/?topic=definition" replace />} />
+          <Route path="overview" element={<Navigate to="/?topic=definition" replace />} />
+          <Route path="definition" element={<Navigate to="/?topic=definition" replace />} />
+          <Route path="traversal" element={<Navigate to="/?topic=traversal" replace />} />
+          <Route path="linked-implementation" element={<Navigate to="/?topic=linked" replace />} />
+          <Route path="binary-tree-operations" element={<Navigate to="/?topic=binary-ops" replace />} />
+          <Route path="bst-operations" element={<Navigate to="/?topic=bst-ops" replace />} />
+          <Route path="multiway-trees" element={<Navigate to="/?topic=multiway" replace />} />
+          <Route path="b-trees" element={<Navigate to="/?topic=btree" replace />} />
+          <Route path="avl-tree" element={<Navigate to="/?topic=avl" replace />} />
+          <Route path="single-rotation" element={<Navigate to="/?topic=avl" replace />} />
+          <Route path="double-rotation" element={<Navigate to="/?topic=avl" replace />} />
 
           <Route path="lab" element={<LabPage />} />
           <Route path="programs" element={<ProgramsPage />} />
           <Route path="practice" element={<PracticePage />} />
           <Route path="revise" element={<RevisionPage />} />
 
-          <Route path="*" element={<Navigate to="/definition" replace />} />
+          <Route path="*" element={<Navigate to="/?topic=definition" replace />} />
         </Route>
       </Routes>
     </BrowserRouter>
