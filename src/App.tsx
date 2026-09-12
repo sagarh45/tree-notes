ebfc8963ef82439b97b1ad332b4cdbf9316ccc5b
sagarh45 <sagarhiremath12@gmail.com>
2026-09-13 00:29:08 +0530

    Remove generic site routes and keep syllabus-point course navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,6 @@
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
 import { AppShell } from './layout/AppShell'
 import { CoursePage } from './pages/CoursePage'
-import { LabPage } from './pages/LabPage'
-import { PracticePage } from './pages/PracticePage'
-import { RevisionPage } from './pages/RevisionPage'
-import { ProgramsPage } from './pages/ProgramsPage'
 
 export default function App() {
   return (
@@ -13,25 +9,25 @@ export default function App() {
         <Route element={<AppShell />}>
           <Route index element={<CoursePage />} />
 
-          <Route path="theory" element={<Navigate to="/?topic=definition" replace />} />
-          <Route path="overview" element={<Navigate to="/?topic=definition" replace />} />
-          <Route path="definition" element={<Navigate to="/?topic=definition" replace />} />
-          <Route path="traversal" element={<Navigate to="/?topic=traversal" replace />} />
-          <Route path="linked-implementation" element={<Navigate to="/?topic=linked" replace />} />
-          <Route path="binary-tree-operations" element={<Navigate to="/?topic=binary-ops" replace />} />
-          <Route path="bst-operations" element={<Navigate to="/?topic=bst-ops" replace />} />
-          <Route path="multiway-trees" element={<Navigate to="/?topic=multiway" replace />} />
-          <Route path="b-trees" element={<Navigate to="/?topic=btree" replace />} />
-          <Route path="avl-tree" element={<Navigate to="/?topic=avl" replace />} />
-          <Route path="single-rotation" element={<Navigate to="/?topic=avl" replace />} />
-          <Route path="double-rotation" element={<Navigate to="/?topic=avl" replace />} />
+          <Route path="definition" element={<Navigate to="/?topic=definition&view=theory" replace />} />
+          <Route path="traversal" element={<Navigate to="/?topic=traversal&view=theory" replace />} />
+          <Route path="linked-implementation" element={<Navigate to="/?topic=linked&view=theory" replace />} />
+          <Route path="binary-tree-operations" element={<Navigate to="/?topic=binary-ops&view=theory" replace />} />
+          <Route path="bst-operations" element={<Navigate to="/?topic=bst-ops&view=theory" replace />} />
+          <Route path="multiway-trees" element={<Navigate to="/?topic=multiway&view=theory" replace />} />
+          <Route path="b-trees" element={<Navigate to="/?topic=btree&view=theory" replace />} />
+          <Route path="avl-tree" element={<Navigate to="/?topic=avl&view=theory" replace />} />
+          <Route path="single-rotation" element={<Navigate to="/?topic=single-rotation&view=theory" replace />} />
+          <Route path="double-rotation" element={<Navigate to="/?topic=double-rotation&view=theory" replace />} />
 
-          <Route path="lab" element={<LabPage />} />
-          <Route path="programs" element={<ProgramsPage />} />
-          <Route path="practice" element={<PracticePage />} />
-          <Route path="revise" element={<RevisionPage />} />
+          <Route path="theory" element={<Navigate to="/?topic=definition&view=theory" replace />} />
+          <Route path="overview" element={<Navigate to="/?topic=definition&view=theory" replace />} />
+          <Route path="lab" element={<Navigate to="/?topic=definition&view=visualization" replace />} />
+          <Route path="programs" element={<Navigate to="/?topic=definition&view=program" replace />} />
+          <Route path="practice" element={<Navigate to="/?topic=definition&view=theory" replace />} />
+          <Route path="revise" element={<Navigate to="/?topic=definition&view=theory" replace />} />
 
-          <Route path="*" element={<Navigate to="/?topic=definition" replace />} />
+          <Route path="*" element={<Navigate to="/?topic=definition&view=theory" replace />} />
         </Route>
       </Routes>
     </BrowserRouter>
