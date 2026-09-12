7cebf651f028c9c7985bc51f2f6a74fbf67ae865
sagarh45 <sagarhiremath12@gmail.com>
2026-09-12 08:58:33 +0530

    Route top navigation directly by syllabus point

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
 import { AppShell } from './layout/AppShell'
 import { SyllabusPage } from './pages/SyllabusPage'
-import { HomePage } from './pages/HomePage'
 import { LabPage } from './pages/LabPage'
 import { ProgramsPage } from './pages/ProgramsPage'
 import { PracticePage } from './pages/PracticePage'
@@ -12,14 +11,28 @@ export default function App() {
     <BrowserRouter>
       <Routes>
         <Route element={<AppShell />}>
-          <Route index element={<SyllabusPage />} />
-          <Route path="theory" element={<SyllabusPage />} />
-          <Route path="overview" element={<HomePage />} />
+          <Route index element={<Navigate to="/definition" replace />} />
+
+          <Route path="definition" element={<SyllabusPage />} />
+          <Route path="traversal" element={<SyllabusPage />} />
+          <Route path="linked-implementation" element={<SyllabusPage />} />
+          <Route path="binary-tree-operations" element={<SyllabusPage />} />
+          <Route path="bst-operations" element={<SyllabusPage />} />
+          <Route path="multiway-trees" element={<SyllabusPage />} />
+          <Route path="b-trees" element={<SyllabusPage />} />
+          <Route path="avl-tree" element={<SyllabusPage />} />
+          <Route path="single-rotation" element={<SyllabusPage />} />
+          <Route path="double-rotation" element={<SyllabusPage />} />
+
+          <Route path="theory" element={<Navigate to="/definition" replace />} />
+          <Route path="overview" element={<Navigate to="/definition" replace />} />
+
           <Route path="lab" element={<LabPage />} />
           <Route path="programs" element={<ProgramsPage />} />
           <Route path="practice" element={<PracticePage />} />
           <Route path="revise" element={<RevisionPage />} />
-          <Route path="*" element={<Navigate to="/" replace />} />
+
+          <Route path="*" element={<Navigate to="/definition" replace />} />
         </Route>
       </Routes>
     </BrowserRouter>
